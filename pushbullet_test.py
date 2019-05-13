9620d20386ffd0b70d1a95822bcf1bfabc0a46b2
vishal-keshav <vishal.keshav.1993@gmail.com>
2019-05-13 21:39:13 +0530

    pushbullet test success

---
+++ pushbullet_test.py
@@ -0,0 +1,44 @@
+import os
+import subprocess
+import time
+
+import requests
+from websocket import create_connection
+from requests.auth import HTTPBasicAuth
+import json
+
+def get_notification(data, token):
+    # Limit the push messeges to just 1 (the latest one)
+    url = "https://api.pushbullet.com/v2/pushes?limit=1"
+    headers = {"Accept": "application/json",
+               "Content-Type": "application/json",
+               "User-Agent": "pyPushBullet"}
+    r = requests.request("GET", url, data=None, params=None, headers=headers,
+                                    files=None, auth=HTTPBasicAuth(token, ""))
+    r.raise_for_status()
+    return r.json()
+
+
+def realtime(token):
+    url = "wss://stream.pushbullet.com/websocket/" + token
+    ws = create_connection(url)
+    while 1:
+        # sleep for some time if we dont want nops too much
+        # nr_seconds = 60 # Sleep for 1 minutes
+        #time.sleep(nr_seconds)
+        data = ws.recv()
+        data = json.loads(data)
+        if data["type"] != "nop":
+            if data["type"] == "tickle":
+                notification = get_notification(data, token)
+                print(notification['pushes'][0]['body'])
+
+#-------------------------------------TEST--------------------------------------
+def main():
+    while 1:
+        print("Do something")
+        time.sleep(2)
+
+if __name__ == "__main__":
+    token = ""
+    realtime(token)
